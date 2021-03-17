c5adf8d52a75af893633606a9250da75c331616a
Womba306 <51053748+Womba306@users.noreply.github.com>
2021-03-17 20:56:42 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
 
-namespace Program
+namespace main
 {
+    public enum Monat
+    {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        December = 12
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите любой текст");
-            string TextToFile = Console.ReadLine();
-            File.WriteAllText("D:\\new_file.txt", $"{TextToFile}");
-            DateTime date1 = new DateTime();
-            File.WriteAllText("D:\\startup.txt", $"{date1}");
-            string path = @"D:\\data.dat";
-            Console.WriteLine("Введите любой текст");
-            string text = Console.ReadLine();
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            Console.WriteLine("Максимальная температура воздуха");
+            string temp1 = Console.ReadLine();
+            Console.WriteLine("Минимальнуя температура воздуха");
+            string temp2 = Console.ReadLine();
+            float temp = ((float.Parse(temp1) + float.Parse(temp2)) / 2);
+            Console.WriteLine($"Средная температура воздуха {temp}");
+            Console.WriteLine("Какой сейчас месяц(номер)");
+            string mon = Console.ReadLine();
+            for (int i = 1; i < 14; i++)
             {
-                byte[] input = Encoding.Default.GetBytes(text);
-                writer.Write(input, 0, input.Length);
-                Console.WriteLine("Текст записан в файл");
-                string textFromFile = Encoding.Default.GetString(input);
-                Console.WriteLine($"Текст из файла: {textFromFile}");
+                if (i==(int.Parse(mon)))
+                {
+                    Console.WriteLine((Monat)i);
+                    if (((Monat)i==Monat.December || (Monat)i==Monat.February || (Monat)i==Monat.January) && temp>0)
+                    {
+                        Console.WriteLine("Дождливая зима");
+                    }
+                }
             }
-        }
-    }
-}
+            
 
+            Console.WriteLine("Введите любое число");
+            string number = Console.ReadLine();
+            if (int.Parse(number) % 2 == 0)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+            
+        }
+
+
+
+
+    
+    }
+
+}
