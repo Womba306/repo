6c7bc4204f5c9dc1e612c5a07ec4648b0f89a993
Womba306 <51053748+Womba306@users.noreply.github.com>
2021-03-21 18:48:48 +0300

    Add files via upload
    GetOut = Main

---
+++ ToHomework/GetOut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToHomework
+{
+    class GetOut
+    {
+        static void Main(string[] args)
+        {
+            ProcessManager proc = new ProcessManager();
+            proc.RunProcess();
+        }
+    }
+}
---
+++ ToHomework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ToHomework
+{
+   
+    class Program
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Program()
+        {
+
+        }
+        public Program(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+    }
+}
+
